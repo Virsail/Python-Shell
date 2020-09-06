47510555e26afc23af23ea945c94534cbf154321
virsail <virsaileric@gmail.com>
2020-09-06 13:15:42 +0300

    add a assertequal method that checks if my credentials have been added to the credentials list

--- user_test.py
+++ user_test.py
@@ -39,3 +39,6 @@ class TestCredentials(unittest.TestCase):
         """
         Test case to check if a new Credentials instance has been initialized correctly
         """
+         self.assertEqual(self.new_credential.account,'Gmail')
+        self.assertEqual(self.new_credential.userName,'Owiti_Charles')
+        self.assertEqual(self.new_credential.password,'yx5Gij43')
