7f57d63435459043d51027eab21f80ded12d8e7e
virsail <virsaileric@gmail.com>
2020-09-06 13:08:14 +0300

    add a test class that defines the tests for credential class'

---
+++ user_test.py
@@ -0,0 +1,33 @@
+import unittest
+from user import User
+from user import Credentials
+
+class TestClass(unittest.TestCase):
+    """
+    A Test class that defines test cases for the User class.
+    """
+    def setUp(self):
+        """
+        Method that runs before each individual test methods run.
+        """
+        self.new_user = User('VirsailMbagaya','XyZ3thf1')
+
+        def test_init(self):
+        """
+        test case to chek if the object has been initialized correctly
+        """
+        self.assertEqual(self.new_user.username,'VirsailMbagaya')
+        self.assertEqual(self.new_user.password,'XyZ3thf1')
+
+        def test_save_user(self):
+        """
+        test case to test if a new user instance has been saved into the User list
+        """
+        self.new_user.save_user()
+        self.assertEqual(len(User.user_list),1)
+
+class TestCredentials(unittest.TestCase):
+    """
+    A test class that defines test cases for credentials class
+    """ 
+    def setUp(self):
