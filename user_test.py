a8aea0b33415e06f6407e1a241d7e31c63ce4213
virsail <virsaileric@gmail.com>
2020-09-06 13:10:19 +0300

    add a doctstring to define my method that runs before each individual credential test method runs

--- user_test.py
+++ user_test.py
@@ -31,3 +31,6 @@ class TestCredentials(unittest.TestCase):
     A test class that defines test cases for credentials class
     """ 
     def setUp(self):
+        """
+        Method that runs before each individual credentials test methods run.
+        """
